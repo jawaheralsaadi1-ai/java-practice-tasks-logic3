d942074f29415e534ffb691a024befeb70827892
Jawaher <8alsaadii@gmail.com>
2026-04-12 16:39:52 +0400

    Add Task3 - blackJack

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@
 
 void main() {
     //Task 1
-
 System.out.println("Task 1 : Result : " + makeBricks(3, 1, 8));
     //Task 2
-
+    System.out.println("Task 2 : Result : " + noTeenSum(2, 1, 14));
     //Task 3
+    System.out.println("Task 3 : Result : " + blackJack(19, 22));
 
     //Task 4
 
@@ -20,8 +20,26 @@ System.out.println("Task 1 : Result : " + makeBricks(3, 1, 8));
      return true;
  }
 
+//Task 2 Method
+public static int noTeenSum(int a, int b, int c) {
 
-//Task 17 method
-//public static boolean isEverywhere(int[] nums, int val) {
+    return fixTeen(a) + fixTeen(b) + fixTeen(c);}
+    public static int  fixTeen ( int n){
+        if (n >= 13 && n <= 19) {
+            if (n == 15 || n == 16) return n;
+            return 0;
+        }
+        return n;
+    }
+    // Task 3 Method
+    public static int blackJack ( int a, int b) {
+    if ( a > 21 && b > 21) return 0;
+    if ( a < 21 || b < 21) {
+        if (a > 21) return a;
+        if (b > 21) return b;
+    }
+   return Math.max( a , b);
 
 
+
+    }
